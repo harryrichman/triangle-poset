6f16ad6b946c7dca96723df9cb5899aa0be35a26
Rohan <rpande@uw.edu>
2023-06-19 11:10:25 -0700

    Function shortening

--- Absolute_Values.py
+++ Absolute_Values.py
@@ -1,11 +1,11 @@
 from sympy import *
 import matplotlib.pyplot as plt
+from Mobius_Matrix import input, triangular_numbers, Mobius_Matrix
 
-number = int(input("Number: "))
-triangular_numbers = ([(x * (x + 1) // 2) for x in range(1, number)])
-def Mobius_Matrix(lst):
-    zeta_array = [[0 if n % m != 0 else 1 for n in lst] for m in lst]
-    return Matrix(zeta_array) ** -1
+input()
+triangular_numbers()
+Mobius_Matrix()
+
 M = Mobius_Matrix(triangular_numbers)
 N = M[0, :].tolist()
 
