0c4d1d0739d364b323ff413ccb10e38e4baef7c1
Rohan <rpande@uw.edu>
2023-06-19 11:56:40 -0700

    Updated code to show each plot per tri. numb.

--- Sum_Absolute_Values.py
+++ Sum_Absolute_Values.py
@@ -3,16 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from Mobius_Matrix import Mobius_Matrix
 from Absolute_Values import abs_value
+from Sum_mobius_values import sum_function
 
 number = int(input("Number: "))
 triangular_numbers = ([(x * (x + 1) // 2) for x in range(1, number+1)])
 
 M = Mobius_Matrix(triangular_numbers)
 N = M[0, :].tolist()
-
-def sum_function(abs_list):
-    sum_list = [sum(abs_list[:i+1]) for i in range(len(abs_list))]
-    return sum_list
 
 S = sum_function(abs_value(N[0]))
 slope = (S[-1] - S[0])/(number - 0)
--- Sum_mobius_values.py
+++ Sum_mobius_values.py
@@ -1,12 +1,9 @@
 from sympy import *
 import matplotlib.pyplot as plt
+from Mobius_Matrix import Mobius_Matrix
+from Mobius_Matrix import triangular_numbers
 
-number = int(input("Number: "))
-triangular_numbers = ([(x * (x + 1) // 2) for x in range(1, number)])
-def Mobius_Matrix(lst):
-    zeta_array = [[0 if n % m != 0 else 1 for n in lst] for m in lst]
-    return Matrix(zeta_array) ** -1
-M = Mobius_Matrix(triangular_numbers)
+M = Mobius_Matrix(triangular_numbers())
 N = M[0, :].tolist()
 
 def sum_function(lst):
@@ -15,4 +12,4 @@ def sum_function(lst):
 
 S = sum_function(N[0])
 plt.plot(S)
-plt.show
+plt.show()
