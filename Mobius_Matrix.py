07acd2b497033263f8b29d4c40ab05776d33d8a9
Rohan <rpande@uw.edu>
2023-06-19 11:10:17 -0700

    Function shortening

--- Mobius_Matrix.py
+++ Mobius_Matrix.py
@@ -1,12 +1,19 @@
 from sympy import *
 import matplotlib.pyplot as plt
 
-number = int(input("Number: "))
-triangular_numbers = ([(x * (x + 1) // 2) for x in range(1, number)])
+def input():
+    number = int(input("Number: "))
+    return number
+
+def triangular_numbers():
+    triangular_numbers = ([(x * (x + 1) // 2) for x in range(1, input())])
+    return triangular_numbers
+
 def Mobius_Matrix(lst):
     zeta_array = [[0 if n % m != 0 else 1 for n in lst] for m in lst]
     return Matrix(zeta_array) ** -1
-M = Mobius_Matrix(triangular_numbers)
+
+M = Mobius_Matrix(triangular_numbers())
 N = M[0, :].tolist()
 
 plt.plot(N[0])
