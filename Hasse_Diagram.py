6bc1c6ad983cb599e46dc0aa912b8c3b65387eee
Rohan <rpande@uw.edu>
2023-09-28 23:13:31 -0700

    Made Hasse Diagram Code in Python

---
+++ Hasse_Diagram.py
@@ -0,0 +1,21 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Create a directed graph
+G = nx.DiGraph()
+
+# Define the predicate function
+def predicate(x, y):
+    return x % y == 0
+
+# Add edges based on the predicate
+for i in range(1, 21):
+    for j in range(1, 21):
+        if i != j and predicate(i, j):
+            G.add_edge(i, j)
+
+# Draw the Hasse diagram
+pos = nx.shell_layout(G)
+nx.draw(G, pos, with_labels=True, node_size=2000, node_color='skyblue', font_size=10, arrows=False)
+plt.axis('off')
+plt.show()
