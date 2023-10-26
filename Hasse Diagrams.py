f0352c873e2fe89be290a7c668bdf848f217d3bf
Rohan <rpande@uw.edu>
2023-10-26 11:16:08 -0700

    Hasse_Diagram_Testing.py

--- Hasse Diagrams.py
+++ Hasse Diagrams.py
@@ -6,12 +6,16 @@ G = nx.DiGraph()
 def predicate(x, y):
     return x % y == 0
 
-for i in range(1, 5):
-    for j in range(1, 5):
+for i in range(1, 21):
+    for j in range(1, 21):
         if i != j and predicate(i, j):
-            G.add_edge(i, j)
+            G.add_edge(j, i)  # Reversed the edge direction
 
-pos = nx.nx_agraph.graphviz_layout(G, prog="dot")
-nx.draw(G, pos, with_labels=True, node_size=2000, node_color='skyblue', font_size=10, arrows=False)
+
+nodesep = 1.0  # Adjust this value to control the spacing
+args = '-Gnodesep={}'.format(nodesep)
+pos = nx.nx_agraph.graphviz_layout(G, prog="dot", args=args)
+
+nx.draw(G, pos, with_labels=True, node_size=2000, node_color='skyblue', font_size=10, arrows=True)
 plt.axis('off')
 plt.show()
