5f752b83e0bea4e60c7b357f0f790a3d22076674
Rohan <rpande@uw.edu>
2023-10-26 11:05:28 -0700

    Testing Hasse Diagram Code!

--- Hasse Diagrams.py
+++ Hasse Diagrams.py
@@ -1,20 +1,16 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# Create a directed graph
 G = nx.DiGraph()
 
-# Define the predicate function
 def predicate(x, y):
     return x % y == 0
 
-# Add edges based on the predicate
-for i in range(1, 21):
-    for j in range(1, 21):
+for i in range(1, 5):
+    for j in range(1, 5):
         if i != j and predicate(i, j):
             G.add_edge(i, j)
 
-# Draw the Hasse diagram
 pos = nx.nx_agraph.graphviz_layout(G, prog="dot")
 nx.draw(G, pos, with_labels=True, node_size=2000, node_color='skyblue', font_size=10, arrows=False)
 plt.axis('off')
