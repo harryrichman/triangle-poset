9adca757ce979d1574de469adbfadc6b00150045
Rohan <rpande@uw.edu>
2023-10-26 11:51:23 -0700

    Fixed Arrows

--- Hasse Diagrams.py
+++ Hasse Diagrams.py
@@ -9,13 +9,13 @@ def predicate(x, y):
 for i in range(1, 21):
     for j in range(1, 21):
         if i != j and predicate(i, j):
-            G.add_edge(j, i)  # Reversed the edge direction
+            G.add_edge(i, j)  # Reversed the edge direction
 
 
-nodesep = 1.0  # Adjust this value to control the spacing
+nodesep = 50  # Adjust this value to control the spacing
 args = '-Gnodesep={}'.format(nodesep)
 pos = nx.nx_agraph.graphviz_layout(G, prog="dot", args=args)
 
-nx.draw(G, pos, with_labels=True, node_size=2000, node_color='skyblue', font_size=10, arrows=True)
+nx.draw(G, pos, with_labels=True, node_size=2000, node_color='skyblue', font_size=10, arrows=True, arrowstyle="<|-")
 plt.axis('off')
 plt.show()
